7d22c944663673dac815ec1dac428e76a502dc97
Daramkun (Windows) <daramkun@daram.pe.kr>
2016-03-30 19:38:32 +0900

    Fixed bug in DeleteWithoutNumbersProcessor

--- Daramkun.DaramRenamer/Processors/Number/DeleteWithoutNumbersProcessor.cs
+++ Daramkun.DaramRenamer/Processors/Number/DeleteWithoutNumbersProcessor.cs
@@ -12,15 +12,31 @@ namespace Daramkun.DaramRenamer.Processors.Number
 		public string Name { get { return "process_delete_without_numbers"; } }
 		public bool CannotMultithreadProcess { get { return false; } }
 
+		public bool IsWordlyProcessing { get; set; } = false;
+
 		public bool Process ( FileInfo file )
 		{
 			if ( file.ChangedFilename.Length == 0 ) return false;
-			List<string> split = new List<string> ( Path.GetFileNameWithoutExtension ( file.ChangedFilename ).Split ( ' ' ) );
-			for ( int i = 0; i < split.Count; ++i )
-				for ( int j = 0; j < split [ i ].Length; ++i )
-					if ( !( split [ i ] [ j ] >= '0' && split [ i ] [ j ] <= '9' ) )
-						split.RemoveAt ( i-- );
-			file.ChangedFilename = string.Join ( " ", split.ToArray () ) + Path.GetExtension ( file.ChangedFilename );
+			if ( !IsWordlyProcessing )
+			{
+				StringBuilder sb = new StringBuilder ();
+				foreach ( char ch in Path.GetFileNameWithoutExtension ( file.ChangedFilename ) )
+					if ( ch >= '0' && ch <= '9' )
+						sb.Append ( ch );
+				file.ChangedFilename = sb.ToString () + Path.GetExtension ( file.ChangedFilename );
+			}
+			else
+			{
+				List<string> split = new List<string> ( Path.GetFileNameWithoutExtension ( file.ChangedFilename ).Split ( ' ' ) );
+				for ( int i = 0; i < split.Count; ++i )
+					for ( int j = 0; j < split [ i ].Length; ++j )
+						if ( !( split [ i ] [ j ] >= '0' && split [ i ] [ j ] <= '9' ) )
+						{
+							split.RemoveAt ( i-- );
+							break;
+						}
+				file.ChangedFilename = string.Join ( " ", split.ToArray () ) + Path.GetExtension ( file.ChangedFilename );
+			}
 			return true;
 		}
 	}

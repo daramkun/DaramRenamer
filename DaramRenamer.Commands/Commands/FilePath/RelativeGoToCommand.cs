097618785b14a86c1d391be09403dcdbcc82a994
진재연(Jin Jae-yeon) <daramkun@live.com>
2021-11-15 23:54:36 +0900

    #26 상대 경로 기능이 정상 동작하지 않는 문제 수정

--- DaramRenamer.Commands/Commands/FilePath/RelativeGoToCommand.cs
+++ DaramRenamer.Commands/Commands/FilePath/RelativeGoToCommand.cs
@@ -7,6 +7,9 @@ namespace DaramRenamer.Commands.FilePath
 	[Serializable, LocalizationKey("Command_Name_RelativeGoTo")]
 	public class RelativeGoToCommand : ICommand
 	{
+		private const string CurrentDirectory = ".";
+		private const string PreviousDirectory = "..";
+
 		public bool ParallelProcessable => true;
 		public CommandCategory Category => CommandCategory.Path;
 
@@ -15,7 +18,19 @@ namespace DaramRenamer.Commands.FilePath
 
 		public bool DoCommand(FileInfo file)
 		{
-			file.ChangedPath = System.IO.Path.Combine(file.ChangedPath, Path);
+			var temp = new List<string>(System.IO.Path.Combine(file.ChangedPath, Path).Split('/', '\\'));
+			temp.RemoveAll(dir => dir == CurrentDirectory || string.IsNullOrEmpty(dir));
+
+			var indexOf = -1;
+			while ((indexOf = temp.IndexOf(PreviousDirectory)) >= 0)
+			{
+				if (indexOf == 0 || indexOf == 1)
+					return false;
+				temp.RemoveAt(indexOf);
+				temp.RemoveAt(indexOf - 1);
+			}
+
+			file.ChangedPath = string.Join(System.IO.Path.DirectorySeparatorChar, temp);
 			return true;
 		}
 	}

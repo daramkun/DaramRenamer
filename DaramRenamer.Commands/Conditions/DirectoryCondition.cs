a4cd5d07a33199bb4f4c5bb279b987ebab3636d8
진재연 (Jin Jae-yeon) <daramkun@live.com>
2024-07-02 23:14:34 +0900

    파일 여부, 디렉토리 여부 조건 추가

---
+++ DaramRenamer.Commands/Conditions/DirectoryCondition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DaramRenamer.Conditions;
+
+[Serializable]
+[LocalizationKey("Condition_IsDirectory")]
+public class DirectoryCondition : ICondition
+{
+    public bool IsSatisfyThisCondition(FileInfo file)
+    {
+        return file.IsDirectory;
+    }
+}
---
+++ DaramRenamer.Commands/Conditions/FileCondition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DaramRenamer.Conditions;
+
+[Serializable]
+[LocalizationKey("Condition_IsFile")]
+public class FileCondition : ICondition
+{
+    public bool IsSatisfyThisCondition(FileInfo file)
+    {
+        return !file.IsDirectory;
+    }
+}
